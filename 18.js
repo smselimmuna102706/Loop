9a649ada6cabed5c45c89bd46e0d5c21e99ae7b7
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-08 23:52:12 +0600

    Loop

---
+++ 18.js
@@ -0,0 +1,42 @@
+
+
+for( var i = 0; i<=10;i++){
+    document.write( "<br/>" +"Bangladesh" +"</br>");
+}
+
+// 1 to 10
+
+for( var i = 0; i<=10;i++){
+    document.write("</br>"+i);
+}
+
+// 1,3,5,7..100
+
+document.write("</br>");
+
+
+for( var i = 1; i<=100;i=i+2){
+    document.write("</br>"+i);
+}
+
+
+// 2,4,6,8...100
+
+document.write("</br>");
+
+
+for( var i = 2; i<=100;i=i+2){
+    document.write("</br>"+i);
+}
+
+
+// 10 to 1
+
+document.write("</br>");
+
+
+
+
+
+
+
